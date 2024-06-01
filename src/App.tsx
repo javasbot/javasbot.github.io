78fedff1edaef17bd802c1bbf35fc58d80f9a371
bestGao <1055652678@qq.com>
2024-06-02 00:23:11 +0800

    feat

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,44 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import classnames from "classnames";
 import { Button } from "antd";
 import style from "./App.module.less";
 
 function App() {
-  const [time, setTime] = useState(new Date().toLocaleString());
+  useEffect(() => {}, []);
+  const nodeRef = useRef<any>();
+  const [show, setShow] = useState(true);
 
   useEffect(() => {
-    const timer = setInterval(() => {
-      setTime(new Date().toLocaleString());
-    }, 1000);
-    // const parent = document.querySelector("#parent");
-    // parent?.addEventListener(
-    //   "click",
-    //   (e) => {
-    //     alert("父亲1");
-    //     e.preventDefault();
-    //     // e.stopPropagation()
-    //   },
-    //   {
-    //     capture: true,
-    //     passive: true,
-    //   }
-    // );
-    // const res = isPalindrome(121);
-    // console.log(res);
-    // const son = document.querySelector("#son");
-    // son?.addEventListener(
-    //   "click",
-    //   (e) => {
-    //     alert("子");
-    //     // e.preventDefault()
-    //     // e.stopPropagation()
-    //   },
-    //   {
-    //     capture: false,
-    //   }
-    // );
-    return () => {
-      clearInterval(timer);
-    };
+    nodeRef.current!.addEventListener("click", () => {
+      console.log("ref点击事件");
+    });
   }, []);
 
-  const [startAni, setStartAni] = useState(false);
+  const click = () => {
+    console.log("react点击事件");
+  };
+
+  const toggle = () => {
+    setShow(!show);
+  };
 
   return (
     <div className={style.App}>
-      <header className={style.header}>头部</header>
-      <div className={style.content}>
-        查看{startAni ? "true" : "false"}
-        <div className={classnames(style.container, startAni ? style.ant : "")}>
-          container内容
+      <header className={style.header}>header</header>
+      <section className={style.container}>
+        <div
+          className={classnames(style.content, !show && style.hidden)}
+          ref={nodeRef}
+          onClick={click}
+        >
+          我是隐藏元素
         </div>
-      </div>
-      <footer className={style.footer}>
-        &copy; {time}
-        <Button
-          onClick={() => {
-            setStartAni(true);
-          }}
-        >
-          开始动画
+        <Button type="primary" className={style.btn} onClick={toggle}>
+          显隐
         </Button>
-        <Button
-          onClick={() => {
-            setStartAni(false);
-          }}
-        >
-          结束动画
-        </Button>
-      </footer>
+        <div className={style.wp}>存在的元素</div>
+      </section>
+      <footer className={style.footer}>footer</footer>
     </div>
   );
 }
