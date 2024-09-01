c1cb6893844c1562f1a78ecf542d21584c938443
bestGao <1055652678@qq.com>
2024-09-01 16:51:33 +0800

    feat(l): base

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     target: "esnext",
     emptyOutDir: true,
   },
-  base: process.env.NODE_ENV === "production" ? `${pkgName}` : "/",
+  base: "/",
   server: {
     port: 3000,
     host: true,
